705d7a6832ccc37e05c26758a08c1b5c705b8d35
Tar Gergő <tar.gergo21@gmail.com>
2024-04-25 15:30:24 +0200

    tesztek

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
+import { masodfokuEgyenletMegoldasaTeszt } from "./tesz.js";
 
-console.log(masodfokuEgyenletMegoldasa(1,2,1))
+console.log(masodfokuEgyenletMegoldasa(1, 2, 1));
 
 // Másodfokú egyenlet calculator online http://eprog.hu/oktatas/javascript/feladat05.html
 
-function masodfokuEgyenletMegoldasa(a, b, c) {
-    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
-    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
-    const eredmeny = {
-        X1: x1,
-        X2: x2,
-    };
+export function masodfokuEgyenletMegoldasa(a, b, c) {
+  const eredmeny = {};
+  let X1;
+  let X2;
+  if (a === 0) {
+    X1 = "a változo nem lehet 0!";
+    X2 = "a változo nem lehet 0!";
+    eredmeny.X1 = X1;
+    eredmeny.X2 = X2;
     return eredmeny;
-}
+  }
+  if (b * b - 4 * a * c < 0) {
+    X1 = "Nincs megoldás!";
+    X2 = "Nincs megoldás!";
+    eredmeny.X1 = X1;
+    eredmeny.X2 = X2;
+    return eredmeny;
+  }
+  let x1 = Math.floor(-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+  let x2 = Math.floor(-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
 
+  X1 = x1;
+  X2 = x2;
+  eredmeny.X1 = X1;
+  eredmeny.X2 = X2;
+  return eredmeny;
+}
+masodfokuEgyenletMegoldasaTeszt();
---
+++ tesz.js
@@ -0,0 +1,35 @@
+import { masodfokuEgyenletMegoldasa } from "./main.js";
+export function masodfokuEgyenletMegoldasaTeszt() {
+  const teszLISTA = [
+    {
+      a: 0,
+      b: 2,
+      c: 3,
+      X1: "a változo nem lehet 0!",
+      X2: "a változo nem lehet 0!",
+    },
+    { a: 1.2, b: 4, c: 5, X1: "Nincs megoldás!", X2: "Nincs megoldás!" },
+    { a: 4, b: 6, c: 2, X1: -0.5, X2: -1 },
+    { a: 1, b: 1, c: 1, X1: "Nincs megoldás!", X2: "Nincs megoldás!" },
+    { a: 1, b: 1, c: 2, X1: "Nincs megoldás!", X2: "Nincs megoldás!" },
+    { a: 1, b: 2, c: 1, X1: -1, X2: -1 },
+    { a: 2, b: 1, c: 1, X1: "Nincs megoldás!", X2: "Nincs megoldás!" },
+    { a: 2, b: 3, c: 2, X1: "Nincs megoldás!", X2: "Nincs megoldás!" },
+    { a: 1, b: 1, c: -1, X1: 0.5, X2: -2 },
+    { a: 1, b: -1, c: 1, X1: "Nincs megoldás!", X2: "Nincs megoldás!" },
+    { a: -1, b: 1, c: 1, X1: -0.6, X2: 1.6 },
+  ];
+  teszLISTA.forEach((tesztEset, index) => {
+    let eredmeny = masodfokuEgyenletMegoldasa(
+      tesztEset.a,
+      tesztEset.b,
+      tesztEset.c
+    );
+    console.assert(
+      tesztEset.X1 === eredmeny.X1 && tesztEset.X2 === eredmeny.X2,
+      `%o`,
+      `szamok: ${tesztEset.a} ${tesztEset.b} ${tesztEset.c} vart: ${tesztEset.X1} ${tesztEset.X2} kapott: ${eredmeny.X1} ${eredmeny.X2}`,
+      `Híbás a ${index + 1}. tesztnél!`
+    );
+  });
+}
